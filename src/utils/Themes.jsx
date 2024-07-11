ee0947a46be2bbe042248e2f30a43c7e4b112f20
ANUDT <152813453+ANUDT@users.noreply.github.com>
2024-07-11 20:06:22 +0100

    styled component

---
+++ src/utils/Themes.jsx
@@ -0,0 +1,29 @@
+export const darkTheme = {
+  background: "#1C1C27",
+  backgroundLight: "#854CE6",
+  Primary: "#19212C",
+  text1: "#C8CFD8",
+  text2: "#F2F5F7",
+  text3: "#626970",
+  text4: "#575C66",
+  card: "#FFFFF",
+  card_Light: "#161616",
+  button: "#854CEF",
+  black: "000000",
+  footerBackground: "#00012B"
+};
+
+export const lightTheme = {
+  background: "#FFFFFF",
+  backgroundLight: "f0f0f0",
+  Primary: "#bela01",
+  text1: "#1111111",
+  text2: "#555555",
+  text3: "#222222",
+  text4: "#777777",
+  card: "#FFFFF",
+  card_Light: "#161616",
+  button: "#854CEF",
+};
+
+
