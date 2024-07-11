c4e0850d649707f3eb564e732d8cd5c10681ea26
ANUDT <152813453+ANUDT@users.noreply.github.com>
2024-07-11 20:53:26 +0100

    header components setting

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,12 +6,15 @@ function Header() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">Anu DT</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/aboutme">About Me</Nav.Link>
+            <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
+            <Nav.Link href="/resume">Resume</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
@@ -20,3 +23,25 @@ function Header() {
 }
 
 export default Header;
+
+// THEN I am presented with the developer's name and navigation with titles corresponding to different sections of the portfolio
+// WHEN I view the navigation titles
+// THEN I am presented with the titles About Me, Portfolio, Contact, and Resume, and the title corresponding to the current section is highlighted
+// WHEN I click on a navigation title
+// THEN the browser URL changes and I am presented with the corresponding section below the navigation and that title is highlighted
+// WHEN I load the portfolio the first time
+// THEN the About Me title and section are selected by default
+// WHEN I am presented with the About Me section
+// THEN I see a recent photo or avatar of the developer and a short bio about them
+// WHEN I am presented with the Portfolio section
+// THEN I see titled images of six of the developer’s applications with links to both the deployed applications and the corresponding GitHub repositories
+// WHEN I am presented with the Contact section
+// THEN I see a contact form with fields for a name, an email address, and a message
+// WHEN I move my cursor out of one of the form fields without entering text
+// THEN I receive a notification that this field is required
+// WHEN I enter text into the email address field
+// THEN I receive a notification if I have entered an invalid email address
+// WHEN I am presented with the Resume section
+// THEN I see a link to a downloadable resume and a list of the developer’s proficiencies
+// WHEN I view the footer
+// THEN I am presented with text or icon links to the developer’s GitHub and LinkedIn profiles, and their profile on a third platform (Stack Overflow, Twitter)
