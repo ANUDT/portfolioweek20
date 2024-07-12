4e3a50f81a222cbf146f1a3327f9767ac75eb07a
ANUDT <152813453+ANUDT@users.noreply.github.com>
2024-07-12 19:30:05 +0100

    hyperlinks and icons imported from bootstrap

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,24 +1,26 @@
-
-// import { Container } from "react-bootstrap";
+import "bootstrap-icons/font/bootstrap-icons.css"
+import ListGroup from 'react-bootstrap/ListGroup';
 
 
 function Footer() {
   return (
     <footer>
-    <div className="col-md-3 mb-md-0 mb-3 display-flex">
-    <h5 className="text-uppercase">Links</h5>
-    <ul className="list-unstyled">
-        <li><a href="#!">Link 1</a></li>
-        <li><a href="#!">Link 2</a></li>
-        <li><a href="#!">Link 3</a></li>
-        <li><a href="#!">Link 4</a></li>
-    </ul>
+      <center>
+        {['lg'].map((breakpoint) => (
+        <ListGroup key={breakpoint} horizontal={breakpoint} className="my-2">
+          <ListGroup.Item><a href="https://github.com/ANUDT/portfolioweek20"><i className="bi bi-github"></i> Github </a></ListGroup.Item>
+          <ListGroup.Item><a href="https://linkedin.com/anudt"><i className="bi bi-linkedin"></i> LinkedIn </a></ListGroup.Item>
+          <ListGroup.Item><a href="mailto:raswarup@gmail.com"><i className="bi bi-envelope"></i> Email </a></ListGroup.Item>
+          <ListGroup.Item><a href="https://twitter.com"><i className="bi bi-twitter-x"></i> Twitter </a></ListGroup.Item>
+        </ListGroup>
+      ))}
+    <div className="footer-copyright text-center py-3">© 2024 Copyright:
+  <a href="https://adt.com/"> adt.com</a>
 </div>
-  <div className="footer-copyright text-center py-3">© 2020 Copyright:
-  <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
-</div>
+</center>
 </footer>
 );
 }
 
 export default Footer;
+
