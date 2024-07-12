31f0382051bfb350ed77ce3a7e136157ed9c7522
ANUDT <152813453+ANUDT@users.noreply.github.com>
2024-07-12 20:58:28 +0100

    contact form bootsrap react with validation

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,27 +1,74 @@
 import banner from '../images/contact_page_banner.jpg';
-import { Container , Form,  Image, Button, Col, Row, InputGroup } from 'react-bootstrap';
+import { Container , Form,  Image, Button, Col, Row,} from 'react-bootstrap';
 import { useState } from 'react';
 
 function Contact() {
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+  
+  
   return (
     <Container>
       <br/>
       <Image src={banner} fluid />;
-      <Form>
-      
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput0">
-          <Form.Label>Please let me know if you wish to know more about me ...</Form.Label>
-          <Form.Control type="text" placeholder="my name" />
+
+
+      <Form noValidate validated={validated} onSubmit={handleSubmit}>
+      <Row className="mb-3">
+        <Form.Group as={Col} md="4" controlId="validationCustom01">
+          <Form.Label>First name</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="First name"
+            defaultValue="your first name..."
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
         </Form.Group>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="name@example.com" />
+        <Form.Group as={Col} md="4" controlId="validationCustom02">
+          <Form.Label>Last name</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="Last name"
+            defaultValue="your last name..."
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
         </Form.Group>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-          <Form.Label>Example textarea</Form.Label>
-          <Form.Control as="textarea" rows={3} />
+      </Row>
+      <Row className="mb-3">
+        <Form.Group as={Col} md="4" controlId="validationCustom01">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="Email"
+            defaultValue="your email address..."
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
         </Form.Group>
-      </Form>
+        <Form.Group as={Col} md="4" controlId="validationCustom02">
+          <Form.Label>Your message</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="Message"
+            defaultValue="your text..."
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        </Form.Group>
+      </Row>
+      <Button type="submit">Validate</Button>
+    </Form>
     </Container>
   )
 }
