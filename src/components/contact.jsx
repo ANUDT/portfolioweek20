bb817ed3f65eda6c8425aa181d623ec264a2b687
ANUDT <152813453+ANUDT@users.noreply.github.com>
2024-07-12 20:47:52 +0100

    Trying to replace mail form bootstrap

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,12 +1,14 @@
 import banner from '../images/contact_page_banner.jpg';
-import { Container , Form,  Image } from 'react-bootstrap';
+import { Container , Form,  Image, Button, Col, Row, InputGroup } from 'react-bootstrap';
+import { useState } from 'react';
 
 function Contact() {
   return (
     <Container>
-      
-      <Form>
+      <br/>
       <Image src={banner} fluid />;
+      <Form>
+      
         <Form.Group className="mb-3" controlId="exampleForm.ControlInput0">
           <Form.Label>Please let me know if you wish to know more about me ...</Form.Label>
           <Form.Control type="text" placeholder="my name" />
