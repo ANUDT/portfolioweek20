d4c117131a5f1d2f798373b80b5050df00bd769e
ANUDT <152813453+ANUDT@users.noreply.github.com>
2024-07-12 18:54:12 +0100

    Portfolio margins, cards edit, layout

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -1,37 +1,31 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import CardGroup from 'react-bootstrap/CardGroup';
 import { project } from '../data/value';
 
 function Portfolio() {
+  const cardStyle = {
+    marginBottom: '20px',
+    marginTop: '20px'
+  };
+
+
   return (
-    <CardGroup>
+
+    <>
       {project.map((project, index) => (
-        <Card key={index} style={{ width: '18rem', margin: '10px' }}>
-          <Card.Img variant="top" src={ project.imageUrl} />
+        <Card key={index} style={cardStyle}>
+          <Card.Img variant="top" src={project.imageUrl} />
           <Card.Body>
             <Card.Title>{project.title}</Card.Title>
             <Card.Text>{project.description}</Card.Text>
-            <Button variant="primary" href={project.github}>GIT HUB LINK</Button>
+            <Button variant="primary" href={project.github}>Github Link</Button>
           </Card.Body>
         </Card>
       ))}
-    </CardGroup>
+    </>
   );
 }
 
 export default Portfolio;
 
 
-// WHEN I am presented with the Portfolio section
-// THEN I see titled images of six of the developer’s applications with links to both the deployed applications and the corresponding GitHub repositories
-// WHEN I am presented with the Contact section
-// THEN I see a contact form with fields for a name, an email address, and a message
-// WHEN I move my cursor out of one of the form fields without entering text
-// THEN I receive a notification that this field is required
-// WHEN I enter text into the email address field
-// THEN I receive a notification if I have entered an invalid email address
-// WHEN I am presented with the Resume section
-// THEN I see a link to a downloadable resume and a list of the developer’s proficiencies
-// WHEN I view the footer
-// THEN I am presented with text or icon links to the developer’s GitHub and LinkedIn profiles, and their profile on a third platform (Stack Overflow, Twitter)
