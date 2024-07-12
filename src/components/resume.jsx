d394aebc6d477da89541b322a95107f7bb8ea701
ANUDT <152813453+ANUDT@users.noreply.github.com>
2024-07-12 20:06:49 +0100

    cv added

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -1,9 +1,31 @@
-
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
 
 function Resume() {
   return (
+    <Container fluid>
+      <Row>
+        <Col><>
       <h1>Resume</h1>
-     );
+      <Card>
+          <Card.Body>
+            <Card.Title>My CV</Card.Title>
+            <Card.Text><i className="bi bi-file-person-fill"></i></Card.Text>
+          </Card.Body>
+        </Card>
+        </>1 of 1</Col>
+      </Row>
+    </Container>
+
+
+
+);
 }
 
 export default Resume;
+
+
+
+
